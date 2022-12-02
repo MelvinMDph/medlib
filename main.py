3ce8847b7a68b9dd81e6fbddbf87c05451cc20f3
MelvinMDph <119584792+MelvinMDph@users.noreply.github.com>
2022-12-02 15:46:19 +0800

    Added potassium deficit calculator

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-#Opens calculator
+#MAIN APPLICATION 
+#Calls functions
+
 if __name__=="__main__":
     print("Welcome to the App!")
 
@@ -8,10 +10,12 @@ def main():
     list()
     tag=input("Formula tag: ")
 
-    if tag == "bmi":
+    if tag.lower() == "bmi":
         bmi_calculator()
-    elif tag == "gfr":
+    elif tag.lower() == "gfr":
         gfr_calculator()
+    elif tag.lower() == "k":
+        k_correction()
     
     new_input=input("Continue? [Y/N]: ").lower()
     if new_input=="y":
--- md_formulas.py
+++ md_formulas.py
@@ -1,6 +1,8 @@
 #FORMULAS IN MEDICINE
+#Defines functions
+
 def list():
-    list=["bmi", "gfr"]
+    list=["bmi", "gfr", "k"]
     print("Tags:", list)
 
 
@@ -36,37 +38,33 @@ def gfr_calculator():
     gender=input("Gender [M/F]: ")
     age=float(input("Age: "))
     creatinine=float(input("Creatinine (mg/dL): "))
-
     K=float(0.7)
-    if gender == "m" or "M":
+    if gender.lower() == "m":
         K=float(0.9)
     
     ScrK=float(creatinine/K)
+    min=1
+    max=1
     if ScrK <=1:
         min=ScrK
-        max=1
-    else:
-        min=1
+    elif ScrK >1:
         max=ScrK
     
     alpha=float(-0.241)
-    if gender == "m" or "M":
+    if gender.lower() == "m":
         alpha=float(-0.302)
     
     constant=float(0.9938)
     negativeexponent=float(-1.200)
-
     mintoalpha=min**alpha
     maxtonegexponent=max**negativeexponent
     constanttoage=constant**age
-    
     femalevariable=float(1.012)
-    if gender == "m" or "M":
+    if gender.lower() == "m":
         femalevariable=1
     
     GFR=142*mintoalpha*maxtonegexponent*constanttoage*femalevariable
     print("eGFR =", round(GFR))
-    
     if GFR <15:
         print("Stage 5, kidney failure")
     elif GFR >=90:
@@ -79,3 +77,31 @@ def gfr_calculator():
         print("Stage 3b, moderately to severely decreased")
     elif GFR >=15 <30:
         print("Stage 4, severely decreased")
+
+def k_correction():
+    print("POTASSIUM DEFICIT CALCULATOR")
+    k_current=float(input("Serum K: "))
+    k_desired=float(input("Desired K: "))
+    k_difference=k_desired-k_current
+    constantvariable=k_difference/0.27
+    deficit=constantvariable*100
+    print("K Deficit =", round(deficit), "mEqs" "\n")
+    kcl_tablets=deficit/10
+    print("Suggestion:" "\n" "May need", round(kcl_tablets), "KCl tab. (10 mEqs/tab),")
+    kcl_iv=deficit/40
+    if k_current >2.5 <3.5:
+        added_k_serum=float(round(round(kcl_iv)*0.4), 2)
+        print("or", round(kcl_iv), "cycles of 40 mEq/L KCl correction." "\n")
+        print(round(kcl_iv), "cycles of 40 mEqs KCl/L will add", added_k_serum, "mEqs K to serum (10 mEqs KCl IV increases serum K by ~0.1 mEq/L.")
+    elif k_current <=2.5:
+        print("or", round(kcl_iv), "cycles of 40 mEq/h KCl correction via central line." "\n")
+        print("K <2.5 mEq/L or symptomatic: 40 mEq/h max infusion rate (central line), continuous ECG monitoring, and K monitoring. Patients may require up to 400 mEq/24h.")
+
+    if kcl_iv >3 <6:
+        print("Give half in 24h, monitof K, correct remaining in 1-2 days." "\n")
+    elif kcl_iv >=6:
+        print("Give <1/2 in 24h, monitor K, correct remaining in 1-2 days." "\n")
+
+    print("K 2.5 - 3.5 mEq/L: 10 mEq/h max infusion rate; 40 mEq/L max concentration; not to exceed 200 mEq dose/24h." "\n")
+
+k_correction()
